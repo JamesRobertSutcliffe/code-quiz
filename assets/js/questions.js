ffbc1508690d4fa13497d44db350d0c1a423f290
James Sutcliffe <jamessutcliffe2012@hotmail.co.uk>
2023-01-20 19:11:17 +0000

    Questions developed

---
+++ assets/js/questions.js
@@ -0,0 +1,12 @@
+console.log('questions');
+
+const q1 = "Which of the following is not a way to declare a javascript variable?";
+// vet // let // var // const
+const q2 = "let x = 10; var y = 10; x = 20; What is the value of x?";
+//10 // 5 // 100 // 20;
+const q3 = "What does the final 'S' in CSS stand for?";
+//site // sheets // slap // show;
+const q4 = "What does the 'H' in HTML stand for?";
+//hyper // hello // hero // head;
+const q5 = "What year was javascript invented?";
+//1970 // 2002 // 1995 // 1996;
